efa69400b6e76608f206e3d3d220e691d85e2d94
Saeed Norouzi <saeednorouzi98@gmail.com>
2023-03-14 09:53:04 +0330

    refactor(stringToArray middleware): add new options
    + add the option of converting string separated by "," to array
    +  add the option of converting single string to array

--- app/http/middlewares/string.to.array.middleware.js
+++ app/http/middlewares/string.to.array.middleware.js
@@ -9,6 +9,12 @@ const stringToArray = function (field) {
             if (typeof req.body[field] === "string") {
                 if (req.body[field].indexOf("#") >= 0)
                     req.body[field] = (req.body[field].split("#")).map(item => item.trim());
+
+                else if (req.body[field].indexOf(",") >= 0)
+                    req.body[field] = (req.body[field].split(",")).map(item => item.trim());
+
+                else
+                    req.body[field] = [req.body[field]];
             } else if ((req.body[field].constructor).toString().toLowerCase().indexOf("array") >= 0)
                 req.body[field] = req.body[field].map(item => item.trim());
         } else {
