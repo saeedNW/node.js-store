1897ecb5e2d42bd156429513c580035c8fc3ee99
Saeed Norouzi <saeednorouzi98@gmail.com>
2023-11-13 16:55:03 +0330

    Feat(template engine): add template engine to application
    Description:
    In order to add add the ability to render web pages to application. I have added the EJS template engine in to application.
    
    Change Logs:
    [Packages] [Added] - Install `ejs` module.
    [Packages] [Added] - Install `express ejs layouts` module.
    [Layouts] [Added] - Add a layout file.
    [Main configs] [Modified] - Initialize template engine configs.

--- app/app.js
+++ app/app.js
@@ -27,6 +27,8 @@ module.exports = class Application {
         console.log(`application running in ${process.env.NODE_ENV} environment`);
         /** initialize application configuration method */
         this.applicationConfiguration();
+        /** initialize application template engine */
+        this.templateEngineInitializer();
         /** initialize application swagger configuration method */
         this.applicationSwaggerConfiguration();
         /** initialize mongodb configuration method */
@@ -173,6 +175,46 @@ module.exports = class Application {
             const value = await redisClient.get("name");
             console.log(value)
         })()
+    }
+
+    templateEngineInitializer() {
+        /** import express-ejs-layouts module */
+        const expressLayouts = require("express-ejs-layouts");
+        /** import path module */
+        const path = require("path");
+
+        /** initialize express-ejs-layouts */
+        this.#app.use(expressLayouts);
+
+        /** initialize View Engine */
+        this.#app.set("view engine", "ejs");
+
+        /** set view files location */
+        this.#app.set("views", path.resolve("./resource/views"));
+
+        /**
+         * enable express-ejs-layouts extractScripts feature.
+         * this feature will extract all html "script" tags and put them at the defined location in the layout.
+         */
+        this.#app.set("layout extractScripts", true);
+
+        /**
+         * enable express-ejs-layouts extractStyles feature.
+         * this feature will extract all html "style" tags and put them at the defined location in the layout.
+         */
+        this.#app.set("layout extractStyles", true);
+
+        /**
+         * define main layout file location
+         * Note.1: default layout.ejs file should be in view files directory,
+         * so if you plan to put them in a subdirectory like "views/layouts",
+         * you should change default layout setting using "layout" middleware.
+         * Note.2: for changing layout default setting the path should be started from
+         * view files directory which in this case it should be "./layouts/main_layout".
+         * Note.3: you can also define and change layout for specific pages during page
+         * render process with "layout" option.
+         */
+        this.#app.set("layout", "./layouts/layout");
     }
 
     /**
